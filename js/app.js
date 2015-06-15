baf17a71fb7f18977be459689dc57b5361390515
Zoe Ames <zoeames@gmail.com>
2015-06-15 15:21:30 -0500

    initial files

---
+++ js/app.js
@@ -0,0 +1,13 @@
+var app = angular.module('myApp', ['ngRoute']);
+
+app.config(['$routeProvider', function ($routeProvider) {
+  $routeProvider
+    .when("/", {templateUrl: "partials/home.html", controller: "MainCtrl"})
+    .when("/register", {templateUrl: "partials/register.html", controller: "AuthCtrl"})
+    .when("/login", {templateUrl: "partials/login.html", controller: "AuthCtrl"})
+    .when("/todo", {templateUrl: "partials/todo.html", controller: "TodoCtrl"})
+    .otherwise("/", {templateUrl: "partials/home.html", controller: "MainCtrl"});
+}]);
+
+
+app.constant('API_URL', 'https://angulartodofire.firebaseio.com/');
---
+++ js/controllers/authController.js
@@ -0,0 +1,3 @@
+app.controller('AuthCtrl', ['$scope', function($scope){
+$scope.title = 'auth'
+}]);
