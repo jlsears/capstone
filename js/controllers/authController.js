b01b4d8ffa901f73d0d21c8af41b1b5c6ee56bd8
Zoe Ames <zoeames@gmail.com>
2015-06-17 15:58:21 -0500

    firebase authentication added

--- js/controllers/authController.js
+++ js/controllers/authController.js
@@ -1,8 +1,24 @@
-app.controller('AuthCtrl', ['$scope', 'Person', function($scope, Person){
-
-    $scope.register = function () {
-      Person.create($scope.user, function () {
-        console.log('user Registered!!!')
+app.controller('AuthCtrl', ['$scope', '$location', 'Auth', function($scope, $location, Auth){
+    $scope.login = function() {
+      Auth.login($scope.user.email, $scope.user.password, function() {
+        $location.path('/todo');
+        $scope.$apply();
       });
     };
-}]);
+
+    $scope.register = function() {
+      Auth.register($scope.user.email, $scope.user.password, function() {
+        Auth.login($scope.user.email, $scope.user.password, function() {
+          $location.path('/login');
+          $scope.$apply();
+        });
+      });
+    };
+
+    $scope.logout=function(){
+      Auth.logout(function() {
+        $location.path('/login');
+        $scope.$apply();
+      });
+    };
+  }]);
