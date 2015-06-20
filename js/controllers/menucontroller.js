223fd20bc24cd17f69cd91b0405170a5b705e737
jlsears <jen.lauren.sears@gmail.com>
2015-06-20 13:51:14 -0500

    logout on menu screen working

--- js/controllers/menucontroller.js
+++ js/controllers/menucontroller.js
@@ -1,4 +1,10 @@
-  app.controller('MenuCtrl', function ($scope) {
+  app.controller('MenuCtrl', ['$scope', '$location', 'Auth', function ($scope, $location, Auth) {
     $scope.message = "Where the menu lives!";
-
+    $scope.logout=function(){
+      Auth.logout(function() {
+        $location.path('/login');
+        $scope.$apply();
   });
+};
+
+  }]);
