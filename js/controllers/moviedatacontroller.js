060b83789285296b8d128a92f265b532fdfefeda
jlsears <jen.lauren.sears@gmail.com>
2015-06-29 22:00:14 -0500

    Actually editing on FB side now, too

--- js/controllers/moviedatacontroller.js
+++ js/controllers/moviedatacontroller.js
@@ -39,7 +39,7 @@
       $scope.movie = movie;
     }
 
-    $scope.submitThis = function() {
+    $scope.submitThis = function(movie) {
       movieListing.$save({
         title: $scope.title,
         director: $scope.director,
